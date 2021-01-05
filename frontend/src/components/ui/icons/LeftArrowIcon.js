3466350d353936e97386c53e33b74c34f065d9de
Oliver Vartiainen <oliver@iki.fi>
2021-01-05 23:28:14 +0200

    Embetter LeftArrowIcon

--- frontend/src/components/ui/icons/LeftArrowIcon.js
+++ frontend/src/components/ui/icons/LeftArrowIcon.js
@@ -4,11 +4,11 @@ export default function LeftArrowIcon({ color }) {
   return html`
     <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
       <g transform="rotate(-45, 12, 12)">
-        <path fill="${color}" d="M 3,2 v 16 h 4 v -16 h -4 Z" />
-        <path fill="${color}" d="M 3,2 v 4 h 16 v -4 h -16 Z" />
+        <path fill="${color}" d="M 2,2 v 16 h 4 v -16 h -4 Z" />
+        <path fill="${color}" d="M 2,2 v 4 h 16 v -4 h -16 Z" />
       </g>
 
-      <path fill="${color}" d="M 4,9 v 4 h 20 v -4 h -20 Z" />
+      <path fill="${color}" d="M 0,10 v 4 h 24 v -4 h -24 Z" />
     </svg>
   `;
 }
